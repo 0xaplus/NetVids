5021194ffcf36eea86629300fc6a2f16045dec7e
aplusakorede <aplusakorede@gmail.com>
2023-11-23 10:29:51 +0100

    Responsiveness

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const IMG_PATH = "https://image.tmdb.org/t/p/w1280/";
 const SEARCHAPI =
   "https://api.themoviedb.org/3/search/movie?&api_key=41ee980e4b5f05f6693fda00eb7c4fd4&query=";
 const main = document.getElementById("section");
+const divContainer = document.getElementsByClassName("container");
 const form = document.getElementById("form");
 const search = document.getElementById("query");
 
@@ -12,35 +13,35 @@ function returnMovies(url) {
   fetch(url).then(res => res.json())
   .then(function(data) {
     data.results.forEach(element => {
-        const div_card = document.createElement('div')
-        div_card.setAttribute('class', 'card')
+      const div_card = document.createElement('div')
+      div_card.setAttribute('class', 'card')
 
-        const div_row = document.createElement('div')
-        div_row.setAttribute('class', 'row')
+      const div_row = document.createElement('div')
+      div_row.setAttribute('class', 'row')
 
-        const div_column = document.createElement('div')
-        div_column.setAttribute('class', 'column')
+      const div_column = document.createElement('div')
+      div_column.setAttribute('class', 'column')
 
-        const image = document.createElement('img')
-        image.setAttribute('class', 'thumbnail')
-        image.setAttribute('id', 'image')
+      const image = document.createElement('img')
+      image.setAttribute('class', 'thumbnail')
+      image.setAttribute('id', 'image')
 
-        const title = document.createElement('h3')
-        title.setAttribute('id', 'title')
+      const title = document.createElement('h3')
+      title.setAttribute('id', 'title')
 
-        const center = document.createElement('center')
+      const center = document.createElement('center')
 
-        title.innerHTML = `${element.title}<br><a href="movie.html?id=${element.id}&title=${element.title}">reviews</a>`
-        image.src = IMG_PATH + element.poster_path;
-        console.log(image.src);
+      title.innerHTML = `${element.title}<br><a href="movie.html?id=${element.id}&title=${element.title}">reviews</a>`
+      image.src = IMG_PATH + element.poster_path;
 
-        center.appendChild(image);
-        div_card.appendChild(center)
-        div_card.appendChild(title)
-        div_column.appendChild(div_card)
-        div_row.appendChild(div_column)
+      center.appendChild(image);
+      div_card.appendChild(center)
+      div_card.appendChild(title)
+      div_column.appendChild(div_card)
+      div_row.appendChild(div_column)
 
-        main.appendChild(div_row)
+      // main.appendChild(div_row)
+      main.appendChild(div_row)
     })
   })
 }
@@ -52,7 +53,7 @@ form.addEventListener('submit', (e) => {
     const searchItem = search.value
 
     if (searchItem) {
-        returnMovies(SEARCHAPI + searchItem)
-        search.value = ''
+      returnMovies(SEARCHAPI + searchItem)
+      search.value = ''
     }
 })
